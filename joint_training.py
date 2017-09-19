2ad0aa19f9c2e8782766380aa291b23312b6d314
SebLu1 <sl767@cam.ac.uk>
2017-09-19 23:30:06 +0100

    overnight calculation prepared

--- joint_training.py
+++ joint_training.py
@@ -14,18 +14,16 @@ if __name__ == '__main__':
 
     if 1:
         recon = Loss_Class()
-        recon.train_L2(1500)
-        recon.end()
-
-    if 0:
-        recon = Loss_Class()
+        recon.train_L2(1000)
         recon.train_class_loss(2000)
         recon.end()
 
         recon2 = Loss_Jointly()
+        recon2.train_L2(1000)
         recon2.train_jointly(2000)
         recon2.end()
 
         recon3 = Loss_Jointly()
+        recon3.train_L2(1000)
         recon3.train_classifier_only(2000)
         recon3.end()
