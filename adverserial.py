6e4dcc2925a05785b5c7564b66b61f95fa8559ec
SebLu1 <sl767@cam.ac.uk>
2017-09-20 09:46:07 +0100

    bug fixed

--- adverserial.py
+++ adverserial.py
@@ -1,10 +1,10 @@
 import ImageRecon_classes as ir
 
 class Loss_adverserial(ir.Adverserial):
+    ## specifies the weight factor of L2 loss in comparison to classification loss
+    weightL2_combinedNorms = 0.3
     model_name = 'Adverserial_Loss'
 
 if __name__ == '__main__':
     recon = Loss_adverserial()
-    recon.train_L2(500)
-    recon.train_adv(5,0,10)
-    recon.train_adv(200, 3, 7)
+    recon.train_adv(200, 5, 5)
--- joint_training.py
+++ joint_training.py
@@ -12,18 +12,18 @@ class Train_Classifier_Only(ir.Classification_Loss):
 
 if __name__ == '__main__':
 
+    training_steps = 1000
     if 1:
         recon = Loss_Class()
-        recon.train_L2(1000)
-        recon.train_class_loss(2000)
+        recon.train_class_loss(training_steps=training_steps)
         recon.end()
 
+    if 1:
         recon2 = Loss_Jointly()
-        recon2.train_L2(1000)
-        recon2.train_jointly(2000)
+        recon2.train_jointly(training_steps)
         recon2.end()
 
-        recon3 = Loss_Jointly()
-        recon3.train_L2(1000)
-        recon3.train_classifier_only(2000)
+    if 1:
+        recon3 = Train_Classifier_Only()
+        recon3.train_classifier_only(training_steps)
         recon3.end()
